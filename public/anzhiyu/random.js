963dd9a9d98ba244a83e9547e208d5b1b0a11db2
Kinder200366 <2463960567@qq.com>
2023-08-13 23:08:53 +0800

    final

--- public/anzhiyu/random.js
+++
@@ -1,1 +0,0 @@
-var posts=["posts/3dsd.html","posts/b7fc.html","posts/3eeb.html","posts/0.html","posts/3emd.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
